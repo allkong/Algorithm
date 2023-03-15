31168cc8b3a9a98bc946c79a65f58f180000d9a4
Dabin Jeong <allempti@gmail.com>
2023-03-15 14:32:16 +0900

    [Silver I] Title: 곱셈, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+
+def cal(a, n):
+    if n == 1:
+        return a % c
+    else:
+        d = cal(a, n//2)
+        if n % 2 == 0:
+            return d * d % c
+        else:
+            return d * d * a % c
+
+print(cal(a, b))
