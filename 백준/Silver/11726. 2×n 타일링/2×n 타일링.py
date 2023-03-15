de2963edb4da76ad6211a107e5b37c6be555f2a1
Dabin Jeong <allempti@gmail.com>
2023-03-15 15:28:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 136 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+rec = [0] * 1001
+rec[1] = 1
+rec[2] = 2
+
+for i in range(3, 1001):
+    rec[i] = rec[i-1] + rec[i-2]
+
+print(rec[n] % 10007)
