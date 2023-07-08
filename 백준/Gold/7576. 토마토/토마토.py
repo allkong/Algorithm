abf2d7ee2e466bd872633b4dba73ab1fc6c178ff
Dabin Jeong <allempti@gmail.com>
2023-07-08 18:58:05 +0900

    [Gold V] Title: 토마토, Time: 440 ms, Memory: 159256 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,45 @@
+from collections import deque 
+import sys
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+
+box = []
+for _ in range(n):
+    box.append(list(map(int, input().split())))
+
+queue = deque()
+for i in range(n):
+    for j in range(m):
+        if box[i][j] == 1:
+            queue.append((i, j))
+
+def bfs():
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if box[nx][ny] == 0:
+                box[nx][ny] = box[x][y] + 1
+                queue.append((nx, ny))
+
+bfs()
+
+ans = 0
+for b in box:
+    for tomato in b:
+        if tomato == 0:
+            print(-1)
+            exit()
+        ans = max(ans, tomato)
+
+print(ans-1)
