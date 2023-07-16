f717569d17a1b746d7a50b0591710fe2950c464c
Dabin Jeong <allempti@gmail.com>
2023-07-16 20:35:37 +0900

    Delete 10814.py

--- 10814.py
+++
@@ -1,8 +0,0 @@
-import sys
-n = int(input())
-member = []
-for i in range(n):
-    member.append(sys.stdin.readline().split())
-member.sort(key=lambda x:int(x[0]))
-for m in member:
-    print(m[0], m[1])
