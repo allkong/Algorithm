0653ed194f17a9eacab7764f5dd02464a35ae0d7
Dabin Jeong <allempti@gmail.com>
2023-07-16 20:35:01 +0900

    Delete 10539.py

--- 10539.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-b_seq = list(map(int, input().split()))
-a_seq = [b_seq[0]]
-for i in range(1, n):
-    a_seq.append(b_seq[i]*(i+1)-sum(a_seq))
-for a in a_seq:
-    print(a, end=' ')
