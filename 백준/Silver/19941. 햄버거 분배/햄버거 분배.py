7f04bc51f6ed6588f04f0b83ec9cd24785103df6
Dabin Jeong <allempti@gmail.com>
2023-08-30 13:31:11 +0900

    [Silver III] Title: 햄버거 분배, Time: 144 ms, Memory: 116796 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -2,21 +2,14 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-li = input()
-h_li = []
-p_li = []
+li = list(input())
 cnt = 0
 for i in range(n):
-    if li[i] == 'H':
-        h_li.append(i)
-    else:
-        p_li.append(i)
-
-for h in h_li:
-    for p in p_li:
-        if abs(p - h) <= k:
-            cnt += 1
-            p_li.remove(p)
-            break
+    if li[i] == 'P':
+        for j in range(max(0, i-k), min(n, i+k+1)):
+            if li[j] == 'H':
+                li[j] = 0
+                cnt += 1
+                break
 
 print(cnt)
