3634a19f7f5bd4ba437ae9c866580558e7c252e2
Dabin Jeong <allempti@gmail.com>
2023-12-02 00:59:20 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/README.md
+++ 프로그래머스/unrated/181932. 코드 처리하기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 코드 처리하기 - 181932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181932) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181932#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 0:53:57
+2023년 12월 6일 0:59:17
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -2,16 +2,8 @@ def solution(code):
     ret = ''
     mode = 0
     for idx in range(len(code)):
-        if mode == 0:
-            if code[idx] == '1':
-                mode = 1
-            elif idx % 2 == 0:
-                ret += code[idx]
-        elif mode == 1:
-            if code[idx] == '1':
-                mode = 0
-            elif idx % 2 != 0:
-                ret += code[idx]              
-    if not ret:
-        ret += 'EMPTY'
-    return ret
+        if code[idx] == '1':
+            mode ^= 1
+        elif idx % 2 == mode:
+            ret += code[idx]
+    return ret if ret else 'EMPTY'
