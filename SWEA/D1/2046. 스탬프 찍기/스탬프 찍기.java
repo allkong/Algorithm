ca7e3f2fe412f8df8f9761253e241ea9ae7f4d8e
Dabin Jeong <allempti@gmail.com>
2024-01-23 17:47:26 +0900

    [D1] Title: 스탬프 찍기, Time: 129 ms, Memory: 19,912 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		for (int cnt = 0; cnt < num; cnt++) {
+			System.out.print("#");
+		}
+	}
+}
