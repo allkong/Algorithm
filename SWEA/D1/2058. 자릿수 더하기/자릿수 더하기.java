bf602ac9c0078f032bc9031eabd9bf1246c4eb7f
Dabin Jeong <allempti@gmail.com>
2024-01-25 16:39:03 +0900

    [D1] Title: 자릿수 더하기, Time: 105 ms, Memory: 19,092 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Solution {
-	public static void main(String args[]) throws Exception {
-		Scanner	sc = new Scanner(System.in);
-		String strNum = sc.nextLine();
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String strNum = br.readLine();
 		int sum = 0;
+		
 		for (int idx = 0; idx < strNum.length(); idx++) {
 			sum += Character.getNumericValue(strNum.charAt(idx));
 		}
