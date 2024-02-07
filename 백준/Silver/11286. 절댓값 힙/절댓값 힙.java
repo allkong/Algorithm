f2ca281a4135b654a5f9ac7fb2467a196ba0af8a
Dabin Jeong <allempti@gmail.com>
2024-02-07 14:59:51 +0900

    [Silver I] Title: 절댓값 힙, Time: 424 ms, Memory: 28424 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -17,8 +17,6 @@ public class Main {
 	
 	public static PriorityQueue<Integer> minHeap;
 	
-	
-	
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
