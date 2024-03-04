d9db3032bdac69443691f1c57db70bddf462db36
Dabin Jeong <allempti@gmail.com>
2024-03-04 15:20:57 +0900

    [D1] Title: 스탬프 찍기, Time: 105 ms, Memory: 17,412 KB -BaekjoonHub

--- SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -1,12 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+	public static BufferedReader br;
+	public static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
 		
-		for (int cnt = 0; cnt < num; cnt++) {
-			System.out.print("#");
+		int count = Integer.parseInt(br.readLine().trim());
+		
+		for (int idx = 0; idx < count; idx++) {
+			sb.append("#");
 		}
+		System.out.println(sb);
 	}
 }
