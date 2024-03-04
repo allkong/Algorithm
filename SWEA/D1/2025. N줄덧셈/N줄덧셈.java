8e729cb51267b098ee5355ab63da5b8a80f72e5c
Dabin Jeong <allempti@gmail.com>
2024-03-04 15:29:32 +0900

    [D1] Title: N줄덧셈, Time: 103 ms, Memory: 18,672 KB -BaekjoonHub

--- SWEA/D1/2025. N줄덧셈/N줄덧셈.java
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -3,13 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Solution {
+	public static BufferedReader br;
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int end = Integer.parseInt(br.readLine());
-		int sum = 0;
-		for (int num = 1; num <= end; num++) {
-			sum += num;
-		}
-		System.out.println(sum);
+		br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine().trim());
+		System.out.println(num * (num + 1) / 2);
 	}
 }
