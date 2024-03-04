fce449a33094b8e7908a1b3ddccebf8e1f9b1b3c
Dabin Jeong <allempti@gmail.com>
2024-03-04 15:58:25 +0900

    [D1] Title: 1대1 가위바위보, Time: 99 ms, Memory: 17,688 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
@@ -3,13 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 1: 가위, 2: 바위, 3: 보
+ * B가 이기는 경우: (1, 2), (2, 3), (3, 1)
+ */
 public class Solution {
+	public static BufferedReader br;
+	public static StringTokenizer st;
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine().trim());
 		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		System.out.println((a % 3 == (b + 1) % 3) ? 'A' : 'B');
+		int one = Integer.parseInt(st.nextToken());
+		int other = Integer.parseInt(st.nextToken());
+		
+		System.out.println(one % 3 == (other + 1) % 3 ? 'A' : 'B');
 	}
 }
