6b746a7e0ba4e82db719abbbcb4a63623d2e40be
Dabin Jeong <allempti@gmail.com>
2024-03-04 15:23:13 +0900

    [D1] Title: 신문 헤드라인, Time: 108 ms, Memory: 17,696 KB -BaekjoonHub

--- SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -1,9 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		System.out.println(str.toUpperCase());
+	public static BufferedReader br;
+	
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine().trim();
+		System.out.println(line.toUpperCase());
 	}
 }
