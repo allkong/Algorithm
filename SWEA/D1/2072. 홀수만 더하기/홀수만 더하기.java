69550632e6157571ce6f9b05a4e022453b7f61de
Dabin Jeong <allempti@gmail.com>
2024-03-05 16:14:24 +0900

    [D1] Title: 홀수만 더하기, Time: 99 ms, Memory: 18,608 KB -BaekjoonHub

--- SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -3,6 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 1. 테스트 케이스를 입력받는다.
+ * 2. 여러 숫자들을 입력받아 홀수면 더한다.
+ *    2-1. oddTotal을 0으로 초기화한다.
+ *    2-2. 입력받은 숫자가 2로 나누어 떨어지지 않으면 홀수이므로 oddTotal에 더한다.
+ *         2-2-1. 2로 나누어 떨어지면 2의 배수이므로 짝수이다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringBuilder sb;
