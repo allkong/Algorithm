ccf25b8f6496fa44936fb3487344e40c184614f3
Dabin Jeong <allempti@gmail.com>
2024-03-05 15:46:21 +0900

    [D1] Title: N줄덧셈, Time: 103 ms, Memory: 18,672 KB -BaekjoonHub

--- SWEA/D1/2025. N줄덧셈/N줄덧셈.java
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -2,6 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 1. 1부터 입력받은 숫자까지의 합을 구한다.
+ * 2. 합 공식을 활용해 계산한다.
+ *    2-1. 1부터 n까지의 합은 2분의 n*(n+1)이다.
+ * @author SSAFY
+ *
+ */
 public class Solution {
 	public static BufferedReader br;
 	
