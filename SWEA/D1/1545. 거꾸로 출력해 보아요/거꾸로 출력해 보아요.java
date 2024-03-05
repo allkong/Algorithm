aa39f8af45faed50d8f23f3f86a8ec1c90f8cacc
Dabin Jeong <allempti@gmail.com>
2024-03-05 15:33:20 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 107 ms, Memory: 17,696 KB -BaekjoonHub

--- SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -2,6 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 1. 숫자를 입력받는다.
+ * 2. 입력받는 숫자를 출력한다.
+ * 3. 숫자를 하나 감소시켜서 출력한다.
+ * 4. 숫자가 0이 될 때까지 3번을 반복한다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringBuilder sb;
