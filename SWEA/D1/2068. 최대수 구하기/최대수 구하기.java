4ea59526937a9c2836af2d7bffe93bc131fd0dae
Dabin Jeong <allempti@gmail.com>
2024-03-05 16:07:41 +0900

    [D1] Title: 최대수 구하기, Time: 104 ms, Memory: 18,252 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -3,6 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 1. 테스트 케이스를 입력받는다.
+ * 2. maxNum 변수를 0으로 초기화한다.
+ * 3. 반복문으로 10개의 숫자를 입력받으면서 maxNum과 비교한다.
+ *    3-1. 입력받은 숫자가 maxNum보다 크면 갱신한다. 
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringBuilder sb;
