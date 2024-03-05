c728040aa183e6814ae69737bb0b131f25d5ec98
Dabin Jeong <allempti@gmail.com>
2024-03-05 15:52:18 +0900

    [D1] Title: 서랍의 비밀번호, Time: 101 ms, Memory: 18,624 KB -BaekjoonHub

--- SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -3,6 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 1. 비밀번호 숫자 4자리를 입력받는다.
+ * 2. 몇 번부터 확인할 건지 입력받는다.
+ * 3. 2번에서 입력받은 숫자부터 차례로 1씩 더해서 비밀번호 숫자 4자리까지 도달한 횟수를 구한다.
+ *    3-1. 횟수 = 비밀번호 숫자 - 시작하는 숫자 + 1
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringTokenizer st;
