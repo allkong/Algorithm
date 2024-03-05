4fba63a2a22ec12c4e06cbb54ab664855737d16e
Dabin Jeong <allempti@gmail.com>
2024-03-05 15:30:22 +0900

    [D1] Title: 아주 간단한 계산기, Time: 101 ms, Memory: 18,532 KB -BaekjoonHub

--- SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -3,6 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 1. 두 수를 입력받는다.
+ * 2. 사칙연산(+, -, *, /) 순으로 연산 결과를 출력한다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringTokenizer st;
@@ -10,10 +14,12 @@ public class Solution {
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		
+		// 두 수를 입력받는다.
 		st = new StringTokenizer(br.readLine().trim());
 		int num1 = Integer.parseInt(st.nextToken());
 		int num2 = Integer.parseInt(st.nextToken());
 		
+		// 사칙연산(+, -, *, /) 순으로 연산 결과를 출력한다.
 		System.out.println(num1 + num2);
 		System.out.println(num1 - num2);
 		System.out.println(num1 * num2);
