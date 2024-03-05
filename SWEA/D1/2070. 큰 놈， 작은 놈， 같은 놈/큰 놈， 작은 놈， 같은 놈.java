6873475080d56d633d28e69398711cf847225a5c
Dabin Jeong <allempti@gmail.com>
2024-03-05 16:10:04 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 98 ms, Memory: 17,936 KB -BaekjoonHub

--- SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
@@ -3,6 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 1. 두 개의 숫자를 입력받는다.
+ * 2. if문을 통해 두 개의 숫자를 비교한다.
+ *    2-1. 첫 번째 숫자가 더 크면 >를 출력한다.
+ *    2-2. 두 번째 숫자가 더 크면 <를 출력한다.
+ *    2-3. 두 수가 같으면 =를 출력한다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringBuilder sb;
@@ -23,11 +30,11 @@ public class Solution {
 			sb.append("#").append(tc).append(" ");
 			
 			if (num1 > num2) {
-				sb.append(">").append("\n");
+				sb.append(">").append("\n"); // 첫 번째 숫자가 더 크면 >를 출력한다
 			} else if (num1 < num2) {
-				sb.append("<").append("\n");
+				sb.append("<").append("\n"); // 두 번째 숫자가 더 크면 <를 출력한다
 			} else {
-				sb.append("=").append("\n");
+				sb.append("=").append("\n"); // 두 수가 같으면 =를 출력한다
 			}
 		}
 		
