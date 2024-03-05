da11001619e41dce338398aecc38d67ee196012e
Dabin Jeong <allempti@gmail.com>
2024-03-05 16:04:24 +0900

    [D1] Title: 중간값 찾기, Time: 103 ms, Memory: 18,612 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
+/**
+ * 1. 여러 숫자들을 입력받아 리스트에 저장한다.
+ * 2. 숫자 리스트를 정렬한다.
+ * 3. 정렬한 리스트의 가운데 값(index: size/2)을 출력한다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringBuilder sb;
@@ -21,7 +26,7 @@ public class Solution {
 			nums.add(Integer.parseInt(st.nextToken()));
 		}
 		
-		Collections.sort(nums);
+		Collections.sort(nums); // 리스트 정렬
 		
 		System.out.println(nums.get(size / 2));
 	}
