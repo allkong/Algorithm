597a654b7526ba08df2d5e623e8b6ba86fd4091c
Dabin Jeong <allempti@gmail.com>
2024-03-05 15:54:14 +0900

    [D1] Title: 신문 헤드라인, Time: 108 ms, Memory: 17,696 KB -BaekjoonHub

--- SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -2,6 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/** 
+ * 1. 입력받은 문자열의 문자들을 모두 대문자로 바꿔서 출력한다.
+ * 2. toUpperCase()를 활용하면 문자열의 모든 문자를 대문자로 바꿔준다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	
