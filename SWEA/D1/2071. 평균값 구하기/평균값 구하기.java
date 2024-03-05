a74c5b9d6d6886e6e83973f9709e97df889d31a2
Dabin Jeong <allempti@gmail.com>
2024-03-05 16:12:37 +0900

    [D1] Title: 평균값 구하기, Time: 98 ms, Memory: 18,396 KB -BaekjoonHub

--- SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -3,6 +3,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 1. 테스트 케이스를 입력받는다.
+ * 2. 여러 개의 숫자를 입력받아 그 합을 구한다.
+ *    2-1. total 변수를 0으로 초기화한다.
+ *         2-1-1. 평균을 구해야 하기 때문에 잃어버리는 값이 없도록 double로 선언한다.
+ *    2-2. 숫자를 입력받을 때마다 해당 숫자를 total에 더한다.
+ * 3. 숫자들의 합(total)에 숫자 개수(10)를 나누어 반올림하여 출력한다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringBuilder sb;
