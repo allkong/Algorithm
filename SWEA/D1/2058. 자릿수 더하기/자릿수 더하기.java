d96179d0586a7dd36ce107286e56663287ce6e90
Dabin Jeong <allempti@gmail.com>
2024-03-05 16:02:34 +0900

    [D1] Title: 자릿수 더하기, Time: 98 ms, Memory: 18,372 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -2,6 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 1. 자연수를 입력받는다.
+ * 2. 각 자릿수의 합을 구한다.
+ *    2-1. 입력받은 숫자의 자릿수만큼 반복해서 숫자의 자리마다 접근한다.
+ *    2-2. charAt()으로 문자열의 문자에 접근하고, Character.getNumericValue()로 문자를 숫자로 변환한다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	
