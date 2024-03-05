a610ab576503153dd72fb8e0abc78bbbb7c2efc8
Dabin Jeong <allempti@gmail.com>
2024-03-05 15:43:49 +0900

    [D1] Title: 더블더블, Time: 103 ms, Memory: 17,428 KB -BaekjoonHub

--- SWEA/D1/2019. 더블더블/더블더블.java
+++ SWEA/D1/2019. 더블더블/더블더블.java
@@ -2,6 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 1. 2의 n제곱을 출력한다.
+ *    1-1. n은 0부터 입력받은 숫자까지이다.
+ * 3. Math.pow()를 활용해 2의 n제곱을 구할 수 있다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringBuilder sb;
