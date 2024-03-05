fd834493ffb3e367c3d962fcee1ec2343c0c7d34
Dabin Jeong <allempti@gmail.com>
2024-03-05 15:36:05 +0900

    [D1] Title: 간단한 N 의 약수, Time: 102 ms, Memory: 17,424 KB -BaekjoonHub

--- SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.java
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 1. 숫자를 입력받는다.
+ * 2. 입력받은 숫자의 약수를 구한다.
+ *    2-1. 1부터 자신까지 입력받은 숫자를 나누었을 때 나누어 떨어지면 약수이다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringBuilder sb;
@@ -15,6 +20,7 @@ public class Solution {
 		int num = Integer.parseInt(br.readLine().trim());
 		
 		for (int idx = 1; idx <= num; idx++) {
+			// 1부터 자신까지 입력받은 숫자를 나누었을 때 나누어 떨어지면 약수이다.
 			if (num % idx == 0) {
 				sb.append(idx).append(" ");
 			}
