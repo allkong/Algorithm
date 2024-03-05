017241b98cdc2535a931449319a6a74ba19c8265
Dabin Jeong <allempti@gmail.com>
2024-03-05 15:59:04 +0900

    [D1] Title: 알파벳을 숫자로 변환, Time: 103 ms, Memory: 19,360 KB -BaekjoonHub

--- SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.java
+++ SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.java
@@ -2,6 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 1. 입력받은 알파벳들을 숫자로 변환하여 출력한다.
+ *    1-1. A가 1로 시작한다.
+ * 2. 아스키코드로 A가 65이기 때문에 -64를 하면 각 알파벳에 해당하는 숫자를 구할 수 있다.
+ */
 public class Solution {
 	public static BufferedReader br;
 	public static StringBuilder sb;
