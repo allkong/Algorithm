dac76945d5b74d2645de2ba1ece5d54de8740421
Dabin Jeong <allempti@gmail.com>
2024-04-11 08:39:44 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,1 +1,5 @@
-SELECT MEMBER_ID, MEMBER_NAME, GENDER, LEFT(DATE_OF_BIRTH, 10) FROM MEMBER_PROFILE WHERE MONTH(DATE_OF_BIRTH) = 3 AND GENDER = 'W' AND NOT TLNO is null;
+SELECT member_id, member_name, gender, DATE_FORMAT(date_of_birth, '%Y-%m-%d') 'date_format'
+FROM member_profile
+WHERE MONTH(date_of_birth) = 3
+AND gender = 'W'
+AND tlno IS NOT NULL;
