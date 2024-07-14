1546e03d56e1309000cd5d5f11de014dcb2547e5
Dabin Jeong <allempti@gmail.com>
2024-07-14 12:24:39 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 56 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -5,13 +5,12 @@ input = sys.stdin.readline
 size, target = map(int, input().split())
 #  1 ~ n 사람 구성
 queue = deque(range(1, size+1))
+josephus_permutation = []
 
-print('<', end='')
 # 모든 사람이 제거될 때까지 계속한다
 while queue:
     # target-1번만큼 왼쪽으로 큐를 이동시킨다
     queue.rotate(-target+1)
-    print(queue.popleft(), end='')
-    if queue:
-        print(', ', end='')
-print('>')
+    josephus_permutation.append(str(queue.popleft()))
+
+print('<' + ', '.join(josephus_permutation) + '>')
