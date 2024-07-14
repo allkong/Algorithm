a6b9c9476055caaca97914e8c31949d0583a1454
Dabin Jeong <allempti@gmail.com>
2024-07-14 16:51:53 +0900

    [Silver III] Title: N과 M (1), Time: 124 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,5 +1,9 @@
+import sys
 from itertools import permutations
+input = sys.stdin.readline
 
-n, m = map(int, input().split())
-for i in permutations(range(1, n+1), m):
-    print(*i)
+element_count, select_count = map(int, input().split()) # 전체 원소 개수, 선택할 원소 개수
+# 순열
+# 전체 원소에서 선택할 숫자만큼 숫자를 뽑는다
+for select_element in permutations(range(1, element_count+1), select_count):
+    print(*select_element)
