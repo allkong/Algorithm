d2758c3b1908d4aaf6a98252f13e8ba79f541706
Dabin Jeong <allempti@gmail.com>
2024-07-21 21:39:40 +0900

    [Silver II] Title: DFS와 BFS, Time: 144 ms, Memory: 114332 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,38 +2,42 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def dfs(graph, v, dfs_visited):
-    dfs_visited[v] = True
-    print(v, end=' ')
-    for i in graph[v]:
-        if not dfs_visited[i]:
-            dfs(graph, i, dfs_visited)
+def dfs(graph, vertex, dfs_visited):
+    # 현재 노드를 방문 처리
+    dfs_visited[vertex] = True
+    print(vertex, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for idx in graph[vertex]:
+        if not dfs_visited[idx]:
+            dfs(graph, idx, dfs_visited)
 
 def bfs(graph, start, bfs_visited):
     queue = deque([start])
+    # 현재 노드를 방문 처리
     bfs_visited[start] = True
     while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for i in graph[v]:
-            if not bfs_visited[i]:
-                queue.append(i)
-                bfs_visited[i] = True
+        # 큐에서 방문할 원소 꺼내기
+        vertex = queue.popleft()
+        print(vertex, end=' ')
+        # 아직 방문하지 않은 인접한 원소들을 큐에 삽입
+        for idx in graph[vertex]:
+            if not bfs_visited[idx]:
+                queue.append(idx)
+                bfs_visited[idx] = True
 
-n, m, v = map(int, input().split())
+nodeCount, edgeCount, vertex = map(int, input().split())
+graph = [[] for _ in range(nodeCount + 1)]
+dfs_visited = [False] * (nodeCount + 1)
+bfs_visited = [False] * (nodeCount + 1)
 
-graph = [[] for i in range(n+1)]
-dfs_visited = [False] * (n+1)
-bfs_visited = [False] * (n+1)
-
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+for _ in range(edgeCount):
+    one, other = map(int, input().split())
+    graph[one].append(other)
+    graph[other].append(one)
 
 for g in graph:
     g.sort()
 
-dfs(graph, v, dfs_visited)
+dfs(graph, vertex, dfs_visited)
 print()
-bfs(graph, v, bfs_visited)
+bfs(graph, vertex, bfs_visited)
