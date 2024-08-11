f8db975630194a56410e26c87df64dddce41ed0c
Dabin Jeong <allempti@gmail.com>
2024-08-11 23:10:41 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,19 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-count = n
+ALPHABET_COUNT = 26 # 알파벳 개수
+ASCII_OFFSET = 97 # 아스키코드에서 a가 97
 
-for _ in range(n):
+test_case = int(input())
+groupword_count = test_case
+
+for _ in range(test_case):
     word = input()
-    for idx in range(0, len(word)-1):
-        # 다음 문자와 같다면 넘어간다
-        if word[idx] == word[idx+1]:
-            continue
-        # 현재 문자가 다음 문자와 다르면서
-        # 이후에 같은 문자가 다시 나온다면 그룹 단어가 아니다
-        elif word[idx] in word[idx+1:]:
-            count -= 1
-            break
+    visited = [False] * ALPHABET_COUNT # 방문 처리 배열
+    visited[ord(word[0]) - ASCII_OFFSET] = True # 첫 알파벳은 무조건 방문 처리한다
 
-print(count)
+    # 한 단어의 모든 알파벳을 살펴본다
+    for alphabetIdx in range(1, len(word) - 1):
+        target = ord(word[alphabetIdx]) - ASCII_OFFSET
+
+        # 이전과 연속되지 않는 알파벳일 때 확인한다
+        if word[alphabetIdx] != word[alphabetIdx-1]:
+            # 방문한 적 없는 알파벳이면 방문 표시를 한다
+            if not visited[target]:
+                visited[target] = True
+            # 방문한 적 있는 알파벳이면 그룹 단어가 아니다
+            else:
+                groupword_count -= 1
+                break
+
+print(groupword_count)
