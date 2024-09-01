591ae68a8ff696efc1f9a55f11681c35f3342c68
Dabin Jeong <allempti@gmail.com>
2024-09-01 14:35:32 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1488 ms, Memory: 118744 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -2,16 +2,19 @@ from bisect import bisect_left, bisect_right
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-have = list(map(int, input().split()))
-have.sort()
-m = int(input())
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+def count_by_range(array, left_value, right_value):
+    # 정렬된 순서를 유지하면서 배열에 right_value를 삽입할 가장 오른쪽 인덱스를 반환
+    right_index = bisect_right(array, right_value)
+    # 정렬된 순서를 유지하면서 배열에 left_value를 삽입할 가장 왼쪽 인덱스를 반환
+    left_index = bisect_left(array, left_value)
+    return right_index - left_index
+
+card_count = int(input())
 cards = list(map(int, input().split()))
+cards.sort()
+target_count = int(input())
+targets = list(map(int, input().split()))
 
-def cnt(a, left_value, right_value):
-	right_index = bisect_right(a, right_value)
-	left_index = bisect_left(a, left_value)
-	return right_index - left_index
-
-for card in cards:
-	print(cnt(have, card, card), end=' ')
+for target in targets:
+    print(count_by_range(cards, target, target), end=' ')
