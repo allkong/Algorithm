39929e6b5b1b5923609558aec288c734c0df563d
Dabin Jeong <allempti@gmail.com>
2024-09-01 14:07:14 +0900

    [Silver IV] Title: 수 찾기, Time: 368 ms, Memory: 47408 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,22 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-n_num = list(map(int, input().split()))
-n_num.sort()
-m = int(input())
-m_num = list(map(int, input().split()))
-
 def binary_search(array, target, start, end):
-    if start > end:
-        return 0
-    mid = (start + end) // 2
-    if array[mid] == target:
-        return 1
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid - 1)
-    else:
-        return binary_search(array, target, mid + 1, end)
+    while start <= end:
+        # 중간지점 찾기
+        mid = (start + end) // 2
+        # 중간지점의 값이 타겟이면 중간지점 반환
+        if array[mid] == target:
+            return 1
+        # 중간지점의 값보다 타겟이 작으면 왼쪽 확인
+        elif target < array[mid]:
+            end = mid - 1
+        # 중간지점의 값보다 타겟이 크면 오른쪽 확인
+        else:
+            start = mid + 1
+    return 0
 
-for num in m_num:
-    print(binary_search(n_num, num, 0, n - 1))
+number_count = int(input())
+numbers = list(map(int, input().split()))
+numbers.sort()
+target_count = int(input())
+targets = list(map(int, input().split()))
+
+for target in targets:
+    print(binary_search(numbers, target, 0, number_count - 1))
