de67d904ffac91751284957fe97739b04dd2c22d
Dabin Jeong <allempti@gmail.com>
2024-09-15 23:26:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,11 +1,17 @@
-n = int(input())
-cnt = 0
-while n>=0:
-    if n%5==0:
-        cnt += n//5
-        print(cnt)
+import sys
+input = sys.stdin.readline
+
+weight = int(input())
+count = 0
+
+while weight >= 0:
+    # 담아야 하는 설탕의 무게가 5의 배수이면 몫만큼 봉지 개수를 더하고 종료한다
+    if weight % 5 == 0:
+        count += weight // 5
+        print(count)
         break
-    n -= 3
-    cnt += 1
+    # 그렇지 않으면 3kg 봉지를 챙기고 봉지 개수를 1 더한다
+    weight -= 3
+    count += 1
 else:
     print(-1)
