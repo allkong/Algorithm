0573fc3c8e7d595abce9b762a049851a61264c07
Dabin Jeong <allempti@gmail.com>
2024-09-16 00:05:00 +0900

    [Silver III] Title: 소수 구하기, Time: 784 ms, Memory: 112316 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,11 +1,20 @@
-m, n = map(int, input().split())
-def prime_number(x):
-    if x == 1:
+import sys
+input = sys.stdin.readline
+
+def prime_number(target):
+    # 1은 소수가 아니다
+    if target == 1:
         return False
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
+    
+    # 2부터 타겟의 제곱근까의 범위 중에 타겟의 약수가 있는지 찾는다
+    for number in range(2, int(target ** 0.5) + 1):
+        # 타겟이 나눠지는 값이 있다면 소수가 아니다
+        if target % number == 0:
             return False
+
     return True
-for i in range(m, n + 1):
-    if prime_number(i):
-        print(i)
+
+min, max = map(int, input().split())
+for number in range(min, max + 1):
+    if prime_number(number):
+        print(number)
