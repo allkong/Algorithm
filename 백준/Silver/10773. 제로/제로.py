f616f1e9d66b9833b9eeb7c695812a41f53457d4
Dabin Jeong <allempti@gmail.com>
2024-09-16 00:22:44 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,13 +1,18 @@
 import sys
+input = sys.stdin.readline
 
 k = int(input())
-st = []
+stack = []
 
 for _ in range(k):
-    n = int(sys.stdin.readline())
-    if n == 0:
-        st.pop()
+    number = int(input())
+    
+    # 0이 아니면 수를 받아 적는다
+    if number:
+        stack.append(number)
+    # 0을 외치면 가장 최근에 쓴 수를 지운다
     else:
-        st.append(n)
+        stack.pop()
 
-print(sum(st))
+# 받아 적은 수의 합을 구한다
+print(sum(stack))
