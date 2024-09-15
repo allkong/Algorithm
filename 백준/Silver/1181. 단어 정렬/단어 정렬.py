06dcba51a63427e2001d7dc227d9d8b92afe034e
Dabin Jeong <allempti@gmail.com>
2024-09-15 23:57:36 +0900

    [Silver V] Title: 단어 정렬, Time: 68 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,7 +1,8 @@
 import sys
-n = int(input())
-sen = [sys.stdin.readline().rstrip() for _ in range(n)]
-sen = list(set(sen))
-sen.sort()
-sen.sort(key=lambda x: len(x))
-print(*sen, sep='\n')
+input = sys.stdin.readline
+
+word_count = int(input())
+words = [input().strip() for _ in range(word_count)]
+words = list(set(words)) # 중복 제거
+words.sort(key=lambda x: (len(x), x)) # 길이가 짧은 순으로 정렬, 길이가 같으면 사전 순으로 정렬
+print(*words, sep='\n')
