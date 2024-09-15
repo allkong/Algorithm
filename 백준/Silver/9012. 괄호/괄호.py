3c4ed0cf62623b37ce98d286daa3346d8410f1dd
Dabin Jeong <allempti@gmail.com>
2024-09-15 23:39:20 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -3,26 +3,22 @@ input = sys.stdin.readline
 
 test_case = int(input())
 for _ in range(test_case):
+    parenthesis_string = input().strip() # 괄호 문자열
     stack = [] # 스택
-    parenthesis_string = input() # 괄호 문자열
-    
+
     for parenthesis in parenthesis_string:
-        # 여는 괄호면 스택에 삽입한다
+        # 여는 괄호라면 스택에 삽입한다
         if parenthesis == '(':
             stack.append(parenthesis)
-        # 닫는 괄호면 스택이 비어있는지 확인하고
-        # 비어있다면 괄호가 완성되지 않으니 멈춘다
-        # 비어있지 않다면 스택에서 pop해서 괄호 쌍을 만든다
-        elif parenthesis == ')':
-            if not stack:
+        # 닫는 괄호라면 스택이 비어있는지 확인한다
+        else:
+            # 스택이 비어있지 않다면 여는 괄호를 꺼내서 괄호 쌍을 완성시킨다
+            if stack:
+                stack.pop()
+            # 스택이 비어있다면 올바른 괄호 문자열(VPS)이 아니다
+            else:
                 stack.append(parenthesis)
                 break
-            else:
-                stack.pop()
-
-    # 스택이 비어있다면 모든 괄호쌍이 완성된 것이고
-    # 스택에 괄호가 남아있다면 올바른 괄호 문자열이 아니다
-    if not stack:
-        print("YES")
-    else:
-        print("NO")
+                
+    # 스택이 비어있다면 올바른 괄호 문자열이고, 비어있지 않다면 올바른 괄호 문자열이 아니다
+    print("YES" if not stack else "NO")
