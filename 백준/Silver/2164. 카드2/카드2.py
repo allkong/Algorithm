1a0980bc9c26e1cc61277f6e156ab7db85d886d2
Dabin Jeong <allempti@gmail.com>
2024-09-16 00:31:52 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 50968 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,13 +1,15 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 n = int(input())
-card = deque(range(1, n + 1))
+cards = deque(range(1, n + 1))
 
-while True:
-    if len(card) == 1:
-        break
-
-    card.popleft()
-    card.append(card.popleft())
-
-print(card[0])
+# 카드가 1장 남을 때까지 반복한다
+while len(cards) > 1:
+    # 제일 위에 있는 카드를 버린다
+    cards.popleft()
+    # 제일 위에 있는 카드를 제일 아래로 옮긴다
+    cards.append(cards.popleft())
+# 마지막에 남은 카드를 출력한다
+print(cards[0])
