16d2b09dbd4f769ed2cf2abe1a0f1e505c705201
Dabin Jeong <allempti@gmail.com>
2024-09-21 22:37:55 +0900

    [Silver II] Title: 랜선 자르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -2,9 +2,7 @@ import sys
 input = sys.stdin.readline
 
 def cut_cable(start, end):
-    global max_length
-
-    while start <= end:
+    while start < end - 1:
         cut_count = 0 # 만든 랜선의 개수
         mid = (start + end) // 2
 
@@ -15,18 +13,16 @@ def cut_cable(start, end):
         # 만든 랜선의 개수가 필요한 랜선의 개수보다 적다면
         # 더 많이 잘라야 한다 (랜선의 길이를 더 짧게 잘라야 한다)
         if cut_count < need_count:
-            end = mid - 1
+            end = mid
         # 만든 랜선의 개수가 필요한 랜선의 개수 이상이라면
         # 현재 만든 랜선 개수를 저장하고
-        # 더 길게 잘라도 가능한지 더 횟수를 적게 잘라본다
+        # 더 길게 잘라도 가능한지 횟수를 더 적게 잘라본다
         else:
-            max_length = mid
-            start = mid + 1
+            start = mid
 
-    return max_length
+    return start
 
 has_count, need_count = map(int, input().split())
 cables = [int(input()) for _ in range(has_count)]
-max_length = 0
 
-print(cut_cable(1, max(cables)))
+print(cut_cable(1, max(cables) + 1))
