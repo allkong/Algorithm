dc5e9fb9e6c42f71afe114d148ddc3fbc0f2a90e
Dabin Jeong <allempti@gmail.com>
2024-09-29 23:29:37 +0900

    [Gold V] Title: A와 B, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,9 +1,19 @@
-s = input()
-t = input()
+import sys
+input = sys.stdin.readline
 
-while t and s != t:
+s = input().rstrip()
+t = input().rstrip()
+
+# 반대로 t를 s로 만든다
+# 1. 문자열의 뒤에 A를 추가한다.
+#    -> 문자열이 A로 끝나면 A를 제거한다.
+# 2. 문자열을 뒤집고 뒤에 B를 추가한다.
+#    -> 문자열이 B로 끝나면 B를 제거하고 문자열을 뒤집는다.
+while t and t != s:
+    # 1번 연산 반대로
     if t[-1] == 'A':
         t = t[:-1]
+    # 2번 연산 반대로
     elif t[-1] == 'B':
         t = t[-2::-1]
 
