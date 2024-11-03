17d823f16e2c1e968a87156ff281609afb99c992
Dabin Jeong <allempti@gmail.com>
2024-11-03 23:26:58 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 120 ms, Memory: 112004 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,44 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def game_start():
+    while(queue):
+        cur_pos, cur_cnt = queue.popleft()
+
+        # 100번 칸에 도착하면 종료한다
+        if (cur_pos == 100):
+            return cur_cnt
+
+        # 주사위를 1부터 6 사이로 굴린다
+        for i in range(1, 7):
+            next = cur_pos + i
+
+            # 주사위를 굴린 결과가 100번 칸을 넘어간다면 이동할 수 없다
+            if (next > 100):
+                break
+
+            # 이미 방문한 곳이면 넘어간다
+            if visited[next]:
+                continue
+
+            # 다음 위치가 사다리랑 뱀 모두 아니면 주사위 칸만큼 이동한다
+            if routes[next] == next:
+                queue.append((next, cur_cnt + 1))
+                visited[next] = True
+
+            # 다음 위치가 사다리거나 뱀이면 타고 이동한다
+            else:
+                queue.append((routes[next], cur_cnt + 1))
+                visited[next] = True
+
+n, m = map(int, input().split()) # 사다리의 수, 뱀의 수
+routes = [i for i in range(101)]
+visited = [False] * 101
+
+for _ in range(n + m):
+    x, y = map(int, input().split())
+    routes[x] = y
+
+queue = deque([(1, 0)])
+print(game_start())
