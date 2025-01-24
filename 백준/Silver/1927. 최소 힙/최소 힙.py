9a0240531609cae86e35c383b9d5dbbecedcac87
Dabin Jeong <allempti@gmail.com>
2025-01-24 21:46:31 +0900

    [Silver II] Title: 최소 힙, Time: 164 ms, Memory: 114204 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -9,9 +9,6 @@ for _ in range(n):
     x = int(input())
 
     if x == 0:
-        if heap:
-            print(heapq.heappop(heap))
-        else:
-            print(0)
+        print(heapq.heappop(heap) if heap else 0)
     else:
         heapq.heappush(heap, x)
