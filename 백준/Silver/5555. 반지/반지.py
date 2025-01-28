d17c66cd40c010654c621bf2e56027472f79eb7d
Dabin Jeong <allempti@gmail.com>
2025-01-29 08:51:51 +0900

    [Silver V] Title: 반지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5555. 반지/반지.py
+++ 백준/Silver/5555. 반지/반지.py
@@ -3,22 +3,11 @@ input = sys.stdin.readline
 
 find = input().rstrip()
 n = int(input())
-length = len(find)
 cnt = 0
 
 for _ in range(n):
     ring = input().rstrip()
-
-    for i in range(len(ring)):
-        target = ''
-
-        if i > len(ring) - length:
-            target = ring[i:] + ring[:(i + length) % len(ring)]
-        else:
-            target = ring[i:i + length]
-
-        if find == target:
-            cnt += 1
-            break
+    if find in ring * 2:
+        cnt += 1
 
 print(cnt)
