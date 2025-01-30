e3ec697b3afacb4e8affd641d767c691bb11d8f9
Dabin Jeong <allempti@gmail.com>
2025-01-31 03:25:27 +0900

    [Gold V] Title: 토마토, Time: 648 ms, Memory: 220368 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,17 +1,6 @@
-from collections import deque 
 import sys
+from collections import deque
 input = sys.stdin.readline
-
-m, n, h = map(int, input().split())
-
-box = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
-
-queue = deque()
-for i in range(h):
-    for j in range(n):
-        for k in range(m):
-            if box[i][j][k] == 1:
-                queue.append((i, j, k))
 
 def bfs():
     dz = [0, 0, 0, 0, -1, 1]
@@ -21,26 +10,43 @@ def bfs():
     while queue:
         z, x, y = queue.popleft()
 
+        # 같은 높이의 상하좌우와 위, 아래를 탐색한다
         for i in range(6):
             nz = z + dz[i]
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nz < 0 or nz >= h or nx < 0 or nx >= n or ny < 0 or ny >= m:
+            # 범위를 벗어나면 탐색하지 않는다
+            if nz < 0 or nz >= height or nx < 0 or nx >= row_size or ny < 0 or ny >= col_size:
                 continue
 
-            if box[nz][nx][ny] == 0:
-                box[nz][nx][ny] = box[z][x][y] + 1
+            # 안익은 토마토가 있는 칸이면 하루를 더해서 기록한다
+            # 다음 토마토를 탐색한다
+            if boxes[nz][nx][ny] == 0:
+                boxes[nz][nx][ny] = boxes[z][x][y] + 1
                 queue.append((nz, nx, ny))
+
+col_size, row_size, height = map(int, input().split())
+boxes = [[list(map(int, input().split())) for _ in range(row_size)] for _ in range(height)]
+queue = deque()
+days = 0 # 토마토가 모두 익을 때까지의 최소 날짜
+
+# 익은 토마토가 있는 칸을 모두 찾아 위치를 저장한다
+for i in range(height):
+    for j in range(row_size):
+        for k in range(col_size):
+            if boxes[i][j][k] == 1:
+                queue.append((i, j, k))
 
 bfs()
 
-ans = 0
-for tomatoes in box:
-    for tomato in tomatoes:
-        if 0 in tomato:
+for box in boxes:
+    for row in box:
+        # 토마토가 모두 익지는 못하는 상황이면 -1을 출력한다
+        if 0 in row:
             print(-1)
             exit(0)
-        ans = max(ans, max(tomato))
+        days = max(days, max(row))
 
-print(ans - 1)
+# 시작이 1이므로 하루는 제외한다
+print(days - 1)
