73508d830b63acf2c0f38d2acbc673d8938032c4
Dabin Jeong <allempti@gmail.com>
2025-02-02 06:02:57 +0900

    [Gold V] Title: 상범 빌딩, Time: 104 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -2,26 +2,23 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+# 북남서동상하
 dz = (0, 0, 0, 0, -1, 1)
 dy = (-1, 1, 0, 0, 0, 0)
 dx = (0, 0, -1, 1, 0, 0)
 
-def bfs():
+def bfs(start):
+    queue = deque([start])
+
     while queue:
         z, y, x, m = queue.popleft()
 
-        # 6방향(북남서동상하)을 탐색한다
+        # 6방향을 탐색한다
         for i in range(6):
-            nz = z + dz[i]
-            ny = y + dy[i]
-            nx = x + dx[i]
+            nz, ny, nx = z + dz[i], y + dy[i], x + dx[i]
 
-            # 다음 위치가 범위를 벗어나면 탐색하지 않는다
-            if nz < 0 or nz >= l or ny < 0 or ny >= r or nx < 0 or nx >= c:
-                continue
-
-            # 이미 방문한 칸이면 탐색하지 않는다
-            if visited[nz][ny][nx]:
+            # 다음 위치가 범위를 벗어나거나 이미 방문한 칸이면 탐색하지 않는다
+            if nz < 0 or nz >= l or ny < 0 or ny >= r or nx < 0 or nx >= c or visited[nz][ny][nx]:
                 continue
 
             # 다음 칸이 출구면 탈출한다
@@ -29,13 +26,12 @@ def bfs():
                 print(f'Escaped in {m + 1} minute(s).')
                 return
 
-            # 다음 칸이 비어있는 칸이거나 출구면 이동한다
+            # 다음 칸이 비어있는 칸이면 이동한다
             if building[nz][ny][nx] == '.':
                 visited[nz][ny][nx] = True
                 queue.append((nz, ny, nx, m + 1))
     
     print('Trapped!')
-    return
 
 while True:
     l, r, c = map(int, input().split()) # 층 수, 행 개수, 열 개수
@@ -52,12 +48,12 @@ while True:
         input()
 
     # 시작 지점 위치를 찾아서 저장한다
+    start = None
     for z in range(l):
         for y in range(r):
             for x in range(c):
                 if building[z][y][x] == 'S':
+                    start = (z, y, x, 0)
                     visited[z][y][x] = True
-                    queue = deque([(z, y, x, 0)])
+                    bfs(start)
                     break
-
-    bfs()
