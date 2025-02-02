a767a28a53a8ff2dada4d3dc9a96d788d2da1f6f
Dabin Jeong <allempti@gmail.com>
2025-02-03 04:16:33 +0900

    [Silver II] Title: 색종이 만들기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,30 +1,26 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-
-# 한 변의 길이가 n일 때, 정사각형 모든 칸의 숫자를 다 더 했을 때
-# 0이면 흰색 종이, n * n이면 파란색 종이, 그 외의 숫자면 더 자르기
 
 def cut(r, c, n): # 시작점 (r, c), 한 변의 길이
     global white, blue
 
-    total = sum(map(sum, [row[c:c + n] for row in paper[r:r + n]]))
+    color = paper[r][c]
 
-    if total == 0:
+    for i in range(r, r + n):
+        for j in range(c, c + n):
+            # 다른 색인 칸이 있으면 종이를 자른다
+            if color != paper[i][j]:
+                n //= 2
+                cut(r, c, n) # 왼쪽 위
+                cut(r, c + n, n) # 오른쪽 위
+                cut(r + n, c, n) # 왼쪽 아래
+                cut(r + n, c + n, n) # 오른쪽 아래
+                return
+
+    if color == 0:
         white += 1
-    elif total == n * n:
+    elif color == 1:
         blue += 1
-    else:
-        # 종이 크기가 한 칸이면 종료한다
-        if n == 1:
-            return
-        # 종이를 자른다
-        n //= 2
-        cut(r, c, n) # 왼쪽 위
-        cut(r, c + n, n) # 오른쪽 위
-        cut(r + n, c, n) # 왼쪽 아래
-        cut(r + n, c + n, n) # 오른쪽 아래
-
 
 n = int(input())
 paper = [list(map(int, input().split())) for _ in range(n)]
