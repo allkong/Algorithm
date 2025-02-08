93c652bd3fff2cb24ecb9dcbd70996646fd69d7f
Dabin Jeong <allempti@gmail.com>
2025-02-08 23:41:43 +0900

    [Silver II] Title: K번째 소수, Time: 476 ms, Memory: 168312 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -1,21 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def is_prime(target):
-    global k
-    
-    for i in range(2, int(target ** 0.5) + 1):
-        if target % i == 0:
-            return False
-
-    k -= 1
-    return True
-
 k = int(input())
-i = 1
+n = 7368787
 
-while k > 0:
-    i += 1
-    is_prime(i)
+# 2부터 n까지의 숫자가 지워졌는지 여부를 나타내는 체
+sieve = [True] * (n + 1)
 
-print(i)
+for i in range(2, n + 1):
+    # 아직 지워지지 않은 경우
+    if sieve[i]:
+        k -= 1
+        # i의 배수를 모두 제거
+        for j in range(i, n + 1, i):
+            if sieve[j]:
+                sieve[j] = False
+
+        # k번째로 지워진 수 찾기
+        if k == 0: 
+            print(i)
+            exit()
