07a7352b0dcb559ea88eda4a4f48d28ae39a3f10
Dabin Jeong <allempti@gmail.com>
2025-02-15 17:25:35 +0900

    [Silver II] Title: 피보나치 인버스, Time: 584 ms, Memory: 554904 KB -BaekjoonHub

---
+++ 백준/Silver/10425. 피보나치 인버스/피보나치 인버스.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+MAX = 10 ** 21000
+fibo = [0, 1]
+num = 0
+# 피보나치 수 배열 만들기
+while num <= MAX:
+    num = fibo[-1] + fibo[-2]
+    fibo.append(num)
+
+t = int(input())
+for _ in range(t):
+    f = int(input()) # 피보나치 수 Fn
+    start, end = 0, len(fibo) - 1
+
+    while start + 1 < end:
+        mid = (start + end) // 2
+
+        # 현재 mid의 피보나치 수가 Fn보다 작으면 오른쪽 탐색
+        if fibo[mid] < f:
+            start = mid
+        # 현재 mid의 피보나치 수가 Fn보다 크면 왼쪽 탐색
+        else:
+            end = mid
+    
+    # end가 1일 때는 인덱스가 가장 큰 2를 출력
+    print(end if end != 1 else 2)
