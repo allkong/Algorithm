025fcb142622b764ec10f9b347e96512e1137605
Dabin Jeong <allempti@gmail.com>
2025-02-16 16:53:03 +0900

    [Silver II] Title: 피보나치 인버스, Time: 576 ms, Memory: 554824 KB -BaekjoonHub

--- 백준/Silver/10425. 피보나치 인버스/피보나치 인버스.py
+++ 백준/Silver/10425. 피보나치 인버스/피보나치 인버스.py
@@ -12,17 +12,17 @@ while num <= MAX:
 t = int(input())
 for _ in range(t):
     f = int(input()) # 피보나치 수 Fn
-    start, end = 0, len(fibo) - 1
+    start, end = 0, len(fibo) # [s, e)
 
     while start + 1 < end:
         mid = (start + end) // 2
 
         # 현재 mid의 피보나치 수가 Fn보다 작으면 오른쪽 탐색
-        if fibo[mid] < f:
+        if fibo[mid] <= f:
             start = mid
         # 현재 mid의 피보나치 수가 Fn보다 크면 왼쪽 탐색
         else:
             end = mid
     
     # end가 1일 때는 인덱스가 가장 큰 2를 출력
-    print(end if end != 1 else 2)
+    print(start if end != 1 else 2)
