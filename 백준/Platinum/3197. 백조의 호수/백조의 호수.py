13791e9890a0f9e182a59c12fa3ab1fb56c7c63d
Dabin Jeong <allempti@gmail.com>
2025-07-03 17:19:12 +0900

    [Platinum V] Title: 백조의 호수, Time: 1408 ms, Memory: 283988 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.py
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.py
@@ -3,15 +3,12 @@ from collections import deque
 input = sys.stdin.readline
 
 # 백조들이 만날 수 있는지 확인하기
-def can_meet():
-    global next_swan_q
-    
-    swan_q = next_swan_q
+def move_swan():
     next_swan_q = deque()
 
     # 오늘 이동 가능한 칸만 탐색하기
-    while swan_q:
-        y, x = swan_q.popleft()
+    while cur_swan_q:
+        y, x = cur_swan_q.popleft()
         
         for i in range(4):
             ny = y + dy[i]
@@ -19,29 +16,26 @@ def can_meet():
             
             # 백조 둘이 만났으면 종료하기
             if (ny, nx) == swan[1]:
-                return True
+                return True, None
             
             # 호수 범위 안에 들고 방문한 적이 없는 칸
             if 0 <= ny < R and 0 <= nx < C and not swan_visited[ny][nx]:
                 # 물 칸이면 이어서 탐색하기
                 if lake[ny][nx] == '.':
-                    swan_q.append((ny, nx))
+                    cur_swan_q.append((ny, nx))
                 # 빙하 칸이면 다음에 이동할 큐에 넣기
                 elif lake[ny][nx] == 'X':
                     next_swan_q.append((ny, nx))
                 swan_visited[ny][nx] = True
 
-    return False
+    return False, next_swan_q
 
 # 빙판 녹이기 (하루)
-def melt():
-    global next_water_q
-    
-    water_q = next_water_q
+def melt_ice():
     next_water_q = deque()
     
-    while water_q:
-        y, x = water_q.popleft()
+    while cur_water_q:
+        y, x = cur_water_q.popleft()
         
         for i in range(4):
             ny = y + dy[i]
@@ -54,39 +48,44 @@ def melt():
                     lake[ny][nx] = '.'
                     next_water_q.append((ny, nx))
                     water_visited[ny][nx] = True
+    
+    return next_water_q
+
+R, C = map(int, input().split())
+lake = [list(input().rstrip()) for _ in range(R)] # 호수 정보
 
 dy = [-1, 1, 0, 0] # 상하좌우
 dx = [0, 0, -1, 1]
 
-R, C = map(int, input().split())
-lake = [list(input().rstrip()) for _ in range(R)] # 호수 정보
 swan = [] # 백조 위치
-days = 0 # 백조들이 만나는 데까지 걸리는 날
-
-water_q = deque() # 물 칸
-next_water_q = deque() # 내일 탐색할 물 칸
+cur_water_q = deque() # 물 칸
 water_visited = [[False] * C for _ in range(R)]
 
-# 백조의 위치와 물 위치 찾기
+# 초기 위치 찾기
 for i in range(R):
     for j in range(C):
         if lake[i][j] in ('.', 'L'):
             if lake[i][j] == 'L':
                 swan.append((i, j))
-            next_water_q.append((i, j))
+            cur_water_q.append((i, j))
             water_visited[i][j] = True
 
-swan_q = deque() # 백조가 오늘 갈 수 있는 칸
-next_swan_q = deque([swan[0]]) # 빙하가 오늘 녹으면 백조가 내일 갈 수 있는 칸
+cur_swan_q = deque([swan[0]]) # 백조가 오늘 갈 수 있는 칸
 swan_visited = [[False] * C for _ in range(R)]
 swan_visited[swan[0][0]][swan[0][1]] = True
 
+days = 0 # 백조들이 만나는 데까지 걸리는 날
+
 while True:
     # 백조들이 만날 수 있는지 확인하고 만났다면 종료하기
-    if can_meet():
+    can_meet, next_swan_q = move_swan()
+    if can_meet:
         print(days)
         break
     
     # 빙판 녹이기
-    melt()
+    cur_water_q = melt_ice()
+    
+    # 하루 마무리하기
+    cur_swan_q = next_swan_q
     days += 1
