7dd01689c549e2bb66f8b446bd8222e53c63e9e9
Dabin Jeong <allempti@gmail.com>
2025-07-19 16:09:27 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -1,22 +1,13 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 num = input().rstrip()
-
 n, i = 1, 0
 while True:
-    queue = deque(list(str(n)))
-
-    while queue and i < len(num):
-        x = queue.popleft()
-
-        if num[i] == x:
+    for s in str(n):
+        if num[i] == s:
             i += 1
-    
-    if i == len(num):
-        break
-
+            if i == len(num):
+                print(n)
+                exit()
     n += 1
-
-print(n)
