94e8ebee39540c6a6bbd54d1853c24b29fa29035
Dabin Jeong <allempti@gmail.com>
2025-08-08 21:06:27 +0900

    [Silver I] Title: OX, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/27970. OX/OX.py
+++ 백준/Silver/27970. OX/OX.py
@@ -1,13 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-pattern = list(input().rstrip())
-s = ''
-
-for p in pattern:
-    if p == 'O':
-        s += '1'
-    else:
-        s += '0'
+s = input().rstrip().replace('O', '1').replace('X', '0')
 
 print(int(s[::-1], 2) % (1_000_000_007))
