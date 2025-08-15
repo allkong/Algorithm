b522aed09d431229ac4f4c4452fcc9576f98ee16
Dabin Jeong <allempti@gmail.com>
2025-08-16 03:23:42 +0900

    [Gold IV] Title: 테트로미노, Time: 156 ms, Memory: 115388 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,26 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-def find_t(y, x):
-    max_total = 0
-    # ㅜ, ㅗ, ㅓ, ㅏ 모양 블록 탐색
-    for case in t_d:
-        total = 0
-
-        for c in case:
-            ny = y + c[0]
-            nx = x + c[1]
-
-            if not (0 <= ny < N) or not (0 <= nx < M):
-                break
-
-            total += board[ny][nx]
-        max_total = max(max_total, total)
-    return max_total
-
 def dfs(y, x, cnt, total):
     global max_total
+    # 남은 블록의 값이 최댓값이어도 max_total을 넘지 못하면 종료
+    if total + max_val * (4 - cnt) <= max_total:
+        return
 
+    # 블록 4개가 연결되면 테트로미노 완성
     if cnt == 4:
         max_total = max(max_total, total)
         return
@@ -31,6 +18,11 @@ def dfs(y, x, cnt, total):
 
         if (0 <= ny < N) and (0 <= nx < M) and not visited[ny][nx]:
             visited[ny][nx] = True
+
+            # ㅜ 모양 블록 처리
+            if cnt == 2:    
+                dfs(y, x, cnt + 1, total + board[ny][nx])
+
             dfs(ny, nx, cnt + 1, total + board[ny][nx])
             visited[ny][nx] = False
 
@@ -38,19 +30,14 @@ N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
 max_total = 0
+max_val = max(map(max, board))
 
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-t_d = [[(0, 0), (0, 1), (0, 2), (1, 1)],
-       [(0, 0), (0, 1), (0, 2), (-1, 1)],
-       [(0, 0), (1, -1), (1, 0), (2, 0)],
-       [(0, 0), (1, 0), (1, 1), (2, 0)]]
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)] # 상하좌우
 
+# 모든 좌표를 시작점으로 탐색
 for i in range(N):
     for j in range(M):
         visited[i][j] = True
-        # ㅜ 모양 블록 찾기
-        max_total = max(max_total, find_t(i, j))
-        # ㅜ를 제외한 나머지 블록 찾기
         dfs(i, j, 1, board[i][j])
         visited[i][j] = False
 
