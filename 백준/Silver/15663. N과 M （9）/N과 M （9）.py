c84f9bd2940f7ecee96035b7b4d4b75b9deb848f
Dabin Jeong <allempti@gmail.com>
2025-08-16 22:36:03 +0900

    [Silver II] Title: N과 M (9), Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -8,19 +8,14 @@ def permutation(depth):
 
     prev = None
     for i in range(N):
-        if selected[i]:
-            continue
+        if num[i] != prev and not selected[i]:
+            prev = num[i]
 
-        if num[i] == prev:
-            continue
+            selected[i] = True
+            path[depth] = num[i]
 
-        prev = num[i]
-
-        selected[i] = True
-        path[depth] = num[i]
-
-        permutation(depth + 1)
-        selected[i] = False
+            permutation(depth + 1)
+            selected[i] = False
 
 N, M = map(int, input().split())
 num = sorted(map(int, input().split()))
