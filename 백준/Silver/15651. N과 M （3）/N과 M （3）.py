9a5fd026138163aed3332c1e67c6ce0cd945455b
Dabin Jeong <allempti@gmail.com>
2025-08-16 20:07:47 +0900

    [Silver III] Title: N과 M (3), Time: 808 ms, Memory: 102412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -2,8 +2,5 @@ import sys
 from itertools import product
 input = sys.stdin.readline
 
-element_count, select_count = map(int, input().split()) # 전체 원소 개수, 선택할 원소 개수
-# 중복 순열
-# 전체 원소에서 선택할 숫자만큼 숫자를 뽑는다
-for select_element in product(range(1, element_count+1), repeat=select_count):
-    print(*select_element)
+N, M = map(int, input().split())
+print('\n'.join(' '.join(map(str, p)) for p in product(range(1, N + 1), repeat=M)))
