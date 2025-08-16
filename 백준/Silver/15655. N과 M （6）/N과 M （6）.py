b27392169506e90608fc5ac240a83ebc6de08c01
Dabin Jeong <allempti@gmail.com>
2025-08-16 21:59:20 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,7 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+num = sorted(map(int, input().split()))
+print('\n'.join(' '.join(map(str, p)) for p in combinations(num, M)))
