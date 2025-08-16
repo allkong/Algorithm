3fe6579f9ca6092c65a58f16b897e9e37470ecb5
Dabin Jeong <allempti@gmail.com>
2025-08-16 15:39:28 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,9 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+element_count, select_count = map(int, input().split()) # 전체 원소 개수, 선택할 원소 개수
+# 조합
+# 전체 원소에서 선택할 숫자만큼 숫자를 뽑는다
+for select_element in combinations(range(1, element_count+1), select_count):
+    print(*select_element)
