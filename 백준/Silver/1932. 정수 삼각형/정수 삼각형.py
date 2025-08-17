3e377194f6a8e75ae1e29eb1b6694491c062e579
Dabin Jeong <allempti@gmail.com>
2025-08-17 15:08:30 +0900

    [Silver I] Title: 정수 삼각형, Time: 104 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -3,12 +3,14 @@ input = sys.stdin.readline
 
 n = int(input())
 triangle = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0] * i for i in range(1, n + 1)]
-dp[0][0] = triangle[0][0]
 
-for i in range(n - 1):
-    for j in range(len(triangle[i])):
-        dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + triangle[i + 1][j])
-        dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + triangle[i + 1][j + 1])
+for i in range(1, n):
+    for j in range(i + 1):
+        if j == 0: # 맨 왼쪽
+            triangle[i][j] += triangle[i - 1][j]
+        elif j == i: # 맨 오른쪽
+            triangle[i][j] += triangle[i - 1][j - 1]
+        else: # 가운데
+            triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
 
-print(max(dp[-1]))
+print(max(triangle[-1]))
