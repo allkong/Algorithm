4b1f7415daae6f2e62dc16d65e89e3b264bf0d5c
Dabin Jeong <allempti@gmail.com>
2025-08-22 03:35:01 +0900

    [Silver II] Title: 새끼치기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17291. 새끼치기/새끼치기.py
+++ 백준/Silver/17291. 새끼치기/새끼치기.py
@@ -7,9 +7,6 @@ dp = [0] * (max(N, 3) + 1)
 dp[1], dp[2], dp[3] = 1, 2, 4
 
 for i in range(4, N + 1):
-    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
-
-    if i % 2 != 0:
-        dp[i] += dp[i - 4]
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] + ((i & 1) * dp[i - 4])
 
 print(dp[N])
