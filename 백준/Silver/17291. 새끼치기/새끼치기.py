1306703772d561155b48319d909bf6739b49385e
Dabin Jeong <allempti@gmail.com>
2025-08-22 03:25:52 +0900

    [Silver II] Title: 새끼치기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17291. 새끼치기/새끼치기.py
+++ 백준/Silver/17291. 새끼치기/새끼치기.py
@@ -2,25 +2,14 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-dp = [0] * (N + 1)
+dp = [0] * (max(N, 3) + 1)
 
-if N == 1:
-    print(1)
-elif N == 2:
-    print(2)
-elif N == 3:
-    print(4)
-else:
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 4
+dp[1], dp[2], dp[3] = 1, 2, 4
 
-    for i in range(4, N + 1):
-        tmp = dp[i - 1] + dp[i - 2] + dp[i - 3]
+for i in range(4, N + 1):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
 
-        if i % 2 == 0:
-            dp[i] = tmp
-        else:
-            dp[i] = tmp + dp[i - 4]
+    if i % 2 != 0:
+        dp[i] += dp[i - 4]
 
-    print(dp[-1])
+print(dp[N])
