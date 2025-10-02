956640cdd6fa30c34a3d811fd2c6cbfd196342b3
Dabin Jeong <allempti@gmail.com>
2025-10-03 01:52:52 +0900

    [Silver III] Title: 두 개의 배열, Time: 732 ms, Memory: 214680 KB -BaekjoonHub

---
+++ 백준/Silver/17124. 두 개의 배열/두 개의 배열.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+
+def binary_search(target):
+    start, end = 0, m - 1
+    close = 10 ** 9 - 1
+    idx = m - 1
+    
+    while start <= end:
+        mid = (start + end) // 2
+
+        # 이전보다 절대값 차이가 더 작다면 close 갱신
+        compare = abs(target - B[mid])
+        if close > compare:
+            close = compare
+            idx = mid
+        
+        elif close == compare and idx > mid:
+            close = compare
+            idx = mid
+
+        # 타겟이 더 작으면 왼쪽 탐색
+        if target < B[mid]:
+            end = mid - 1
+
+        # 타겟이 더 크면 오른쪽 탐색
+        else:
+            start = mid + 1
+            
+    return B[idx]
+
+T = int(input())
+for _ in range(T):
+    n, m = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    C = []
+    B.sort()
+
+    for a in A:
+        C.append(binary_search(a))
+    
+    print(sum(C))
