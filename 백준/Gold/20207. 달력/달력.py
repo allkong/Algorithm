956dfec927db3eb235cdf3cce84b500c55110480
Dabin Jeong <allempti@gmail.com>
2025-10-03 21:57:55 +0900

    [Gold V] Title: 달력, Time: 104 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.py
+++ 백준/Gold/20207. 달력/달력.py
@@ -11,20 +11,17 @@ for _ in range(N):
     for i in range(S, E + 1):
         calendar[i] += 1
 
-w, h = 0, 1
-area = 0 # 코딩지 면적
-day = min(calendar.keys())
-last = max(calendar.keys())
+w, h, area = 0, 0, 0
+start = min(calendar.keys())
+end = max(calendar.keys())
 
-while day <= last:
+for day in range(start, end + 1):
     if calendar[day] != 0:
         w += 1
         h = max(h, calendar[day])
 
-    if calendar[day] == 0 or (day == last and calendar != 0):
+    if calendar[day] == 0 or (day == end and calendar != 0):
         area += w * h
-        w, h = 0, 1
-    
-    day += 1
+        w, h = 0, 0
 
 print(area)
